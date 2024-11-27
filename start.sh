8b8ab2c76bd45ec665919038155be1697740ab5e
Lorenzo Pellegrini <0lorenzo.pellegrini0@gmail.com>
2024-11-27 21:39:53 +0100

    Improved readability

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 echo "What are the name of the rabbit file?"
 read name
-echo "What are the name of the infected HDD?"
-read hdd
-php remove_rabbit.php $hdd $name
+
+echo "What are the name of the infected Drive?"
+read drive
+
+php remove_rabbit.php $drive $name
