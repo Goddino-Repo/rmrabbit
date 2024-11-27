48b65c370f988aff914f8c57113a9d957c11135c
Lorenzo Pellegrini <0lorenzo.pellegrini0@gmail.com>
2024-11-27 21:40:07 +0100

    improved readability

--- remove_rabbit.php
+++ remove_rabbit.php
@@ -1,21 +1,27 @@
 <?php
     echo "\nTool for removing rabbit\n";
+    
     $disk = strtolower($argv[1]);
-    if(strpos(" ", $argv[2]))
-    {
-	$argv[2] = str_replace(" ", "\ ", $argv[2]);
+    
+    if(strpos(" ", $argv[2])) {
+	    $argv[2] = str_replace(" ", "\ ", $argv[2]);
     }
-    $testo = shell_exec("find /mnt/" . $disk . "/ -name '" . $argv[2] . "'");
+
+    $text = shell_exec("find /mnt/" . $disk . "/ -name '" . $argv[2] . "'");
+
     $file = fopen("result.txt", "w");
-    fwrite($file, $testo);
+    fwrite($file, $text);
     fclose($file);
-    $lettura = file("result.txt");
-    foreach($lettura as $directory)
-    {
-	if(strpos(" ", $directory))
-	    $directory = str_replace(" ", "\ ", $directory);
-	shell_exec("rm -f " . $directory);
-	echo "deleted " . $directory . "\n";
+    
+    $handle = file("result.txt");
+    foreach($handle as $directory) {
+	    if(strpos(" ", $directory)) {
+            $directory = str_replace(" ", "\ ", $directory);
+        }
+	    
+        shell_exec("rm -f " . $directory);
+	    echo "deleted " . $directory . "\n";
     }
+    
     echo "\nRabbit deleted!\n";
 ?>
